41613e70d9bb626ae75cf2a347510f5102ddd7e0
Anderson <andersonpds14@gmail.com>
2019-05-20 09:19:04 -0300

    Adicionado conteudos de Datas, Excecoes e Strings

---
+++ meu-primeiro-projeto/src/com/algaworks/curso/fjoo/excessoes/SaldoInsuficienteException.java
@@ -0,0 +1,9 @@
+package com.algaworks.curso.fjoo.excessoes;
+
+public class SaldoInsuficienteException extends Exception {
+
+	public SaldoInsuficienteException(String mensagem) {
+		super(mensagem);
+	}
+
+}
